4927d57f4f0c724395d7ef53db7ca71655cffa7e
FizaAamir <144325138+FizaAamir@users.noreply.github.com>
2025-02-07 10:45:27 +0500

    Update main.py

--- fastapi-testapp/main.py
+++ fastapi-testapp/main.py
@@ -1,3 +1,4 @@
+import os
 import uvicorn
 from fastapi import FastAPI
 
@@ -8,4 +9,5 @@ def read_root():
     return {"message": "Hello, Cloud Run!"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))  # Use the PORT environment variable
+    uvicorn.run(app, host="0.0.0.0", port=port)
