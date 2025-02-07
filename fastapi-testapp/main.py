327bad6360a2058f988d016fac89622d173f9b7d
FizaAamir <144325138+FizaAamir@users.noreply.github.com>
2025-02-07 10:20:39 +0500

    Update main.py

--- fastapi-testapp/main.py
+++ fastapi-testapp/main.py
@@ -1,7 +1,11 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, Cloud Run!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
